aabf033572adb1fe4a5b6318c6f8a33943606751
Packer Gaël <gal.packer@gmail.com>
2016-05-19 14:41:22 +0200

    Revert "okok"
    This reverts commit 74c4013d04987923f245896463d88de74b341199.

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -52,9 +52,9 @@ HTMLActuator.prototype.clearContainer = function (container) {
 
 HTMLActuator.prototype.addTile = function (tile) {
   var valueMap = {
-    2 :    '<Udacity>',
+    2 :    '',
     4 :    '<Intro CS>',
-    8 :    "skills=['code']",
+    8 :    "",
     16 :   "skills.add('CSS')",
     32 :   '</Intro CS>',
     64 :   '<Job search>',
@@ -172,8 +172,12 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   tweet.setAttribute("href", "https://twitter.com/share");
   tweet.textContent = "Tweet";
 
-  var text = "" + this.score + " points in Udacity2048! http://ow.ly/vpoFS Code your own game in their new mini course http://ow.ly/vpaLY #2048game"
+  var text = "" + this.score + " points in Balelec2048! #2048game"
   tweet.setAttribute("data-text", text);
 
   return tweet;
 };
+
+
+
+
